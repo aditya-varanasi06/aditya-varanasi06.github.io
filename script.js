1ad1648c20f67c48502996bbced3a4041f54bf44
aditya-varanasi06 <avarana2@student.gitam.edu>
2026-04-17 19:48:04 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function search(){
+alert('Search functionality coming soon!');
+}
+
+function viewDetails(){
+alert('Viewing property details...');
+}
+
+function submitForm(e){
+e.preventDefault();
+alert('Message Sent Successfully!');
+}
